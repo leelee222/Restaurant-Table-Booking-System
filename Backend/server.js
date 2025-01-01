b0ad6752d2c71f4dd243609368b438b6cfcbb3f1
ilham <ilham.adios@epitech.eu>
2025-01-01 20:28:50 +0100

    adding corrections

--- Backend/server.js
+++ Backend/server.js
@@ -16,6 +16,8 @@ const BUSINESS_HOURS = {
   end: "8:00 PM"
 };
 
+const MAX_ADVANCE_DAYS = 30;
+const SAME_DAY_CUTOFF_HOURS = 2;
 
 const isValidTimeSlot = (time) => {
   const validSlots = ["12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", "8:00 PM"];
